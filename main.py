3d3baa450736dce21cfb449262adad1b415ca34c
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2026-09-15 09:39:20 +0700

    Update(Main): Add AI API

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+from groq import AsyncGroq
 
+
+load_dotenv()
 app = FastAPI(tittle = "Chatbot Engine")
+
+client = AsyncGroq()
 
 class ChatRequest(BaseModel):
     user_message: str
@@ -11,7 +18,21 @@ class ChatResponse(BaseModel):
     
 @app.post("/chat", response_model=ChatResponse)
 async def chat_endpoint(request: ChatRequest):
-
-    reply_text = f"Pesan Anda diterima: '{request.user_message}'. (Logika AI belum aktif)"
+    messages = [
+        {"role": "system", "content": "Anda adalah asisten AI yang ramah, ringkas, dan sangat membantu."},
+        {"role": "user", "content": request.user_message}
+    ]
     
-    return ChatResponse(bot_reply=reply_text)
+    try:
+        chat_completion = await client.chat.completions.create(
+            messages=messages,
+            model="groq/compound-mini", 
+            temperature=0.7,       
+        )
+        
+        bot_reply = chat_completion.choices[0].message.content
+        
+    except Exception as e:
+        bot_reply = f"Maaf, terjadi kesalahan saat menghubungi AI: {str(e)}"
+    
+    return ChatResponse(bot_reply=bot_reply)
