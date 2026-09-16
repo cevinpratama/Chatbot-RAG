407a5320aeb4f40e791e341d409f8809ecad7593
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2026-09-16 10:40:58 +0700

    feat: add main program route

--- app/main.py
+++ app/main.py
@@ -1,38 +1,19 @@
-import os
 from fastapi import FastAPI
-from pydantic import BaseModel
 from dotenv import load_dotenv
-from groq import AsyncGroq
-
-
 load_dotenv()
-app = FastAPI(tittle = "Chatbot Engine")
 
-client = AsyncGroq()
+from app.api.chat import router as chat_router
 
-class ChatRequest(BaseModel):
-    user_message: str
 
-class ChatResponse(BaseModel):
-    bot_reply: str
-    
-@app.post("/chat", response_model=ChatResponse)
-async def chat_endpoint(request: ChatRequest):
-    messages = [
-        {"role": "system", "content": "Anda adalah asisten AI yang ramah, ringkas, dan sangat membantu."},
-        {"role": "user", "content": request.user_message}
-    ]
-    
-    try:
-        chat_completion = await client.chat.completions.create(
-            messages=messages,
-            model="groq/compound-mini", 
-            temperature=0.7,       
-        )
-        
-        bot_reply = chat_completion.choices[0].message.content
-        
-    except Exception as e:
-        bot_reply = f"Maaf, terjadi kesalahan saat menghubungi AI: {str(e)}"
-    
-    return ChatResponse(bot_reply=bot_reply)
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Enterprise RAG Chatbot API",
+        description="Backend chatbot terskala dengan arsitektur modular.",
+        version="1.0.0"
+    )
+
+    app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
+
+    return app
+
+app = create_app()
