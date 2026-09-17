ba50652f057f765c3353773ed00cbe4380ab5ea5
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2026-09-17 10:56:08 +0700

    feat: add rag function to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+import os
 from dotenv import load_dotenv
 load_dotenv()
+from app.services.rag import rag_manager
 
 from app.api.chat import router as chat_router
 
@@ -11,6 +13,8 @@ def create_app() -> FastAPI:
         description="Backend chatbot terskala dengan arsitektur modular.",
         version="1.0.0"
     )
+    data_file_path = os.path.join(os.getcwd(), "data", "datarag.txt")
+    rag_manager.ingest_document(data_file_path)
 
     app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
 
